41ac296494e4d7d7ac19d7052983d5b8651d19dc
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-14 17:26:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
-  // write your code here
+
+ shortHex = shortHex.replace("#","").toUpperCase();
+	const red = shortHex[0];
+	const green = shortHex[1];
+	const blue = shortHex[2]
+	const fullHex = `${red.repeat(2)}${green.repeat(2)}${blue.repeat(2)}`
+	return fullHex;
+	
 };
 
 // Do not change the code below.
